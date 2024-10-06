3bbc1888e20193ec976132da5c1bd8573acc7fd1
Ryan Frost-Garant <rfros048@uottawa.ca>
2024-10-05 20:27:09 -0400

    navigation figured out

--- carry-my-luggage-ui/src/App.jsx
+++ carry-my-luggage-ui/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
 import Dashboard from './assets/Dashboard';
 import './App.css';
 import {
@@ -19,20 +19,20 @@ function App() {
 
     <BrowserRouter>
 
-      <div>
+      <div style={{position:"fixed", left:"1%", top:"2%"}}>
         <DropdownMenu>
-          <DropdownMenuTrigger>Open</DropdownMenuTrigger>
-          <DropdownMenuContent>
-            <DropdownMenuLabel>My Account</DropdownMenuLabel>
-            <DropdownMenuSeparator />
+          <DropdownMenuTrigger style={{fontSize:"120%" }}>Navigation Menu</DropdownMenuTrigger>
+          <DropdownMenuContent style={{fontSize:"105%" }}>
               <DropdownMenuItem>
-                <Button variant="outline" href="/Dashboard">
-                  Dashboard
+                <Button>
+                  <Link style={{color:"black"}} to="/">Home</Link>
                 </Button>
               </DropdownMenuItem>
-            <DropdownMenuItem>Billing</DropdownMenuItem>
-            <DropdownMenuItem>Team</DropdownMenuItem>
-            <DropdownMenuItem>Subscription</DropdownMenuItem>
+              <DropdownMenuItem>
+                <Button>
+                  <Link style={{color:"black"}}to="/Dashboard">Dashboard</Link>
+                </Button>
+              </DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
       </div>
@@ -41,6 +41,7 @@ function App() {
       <div>
 
         <Routes>
+          <Route path="/" element = {<this/>} />
           <Route path="/Dashboard" element = {<Dashboard/>} />
         </Routes>
 
--- carry-my-luggage-ui/src/assets/Dashboard.jsx
+++ carry-my-luggage-ui/src/assets/Dashboard.jsx
@@ -6,7 +6,7 @@ function Dashboard(){
     return(
         
         <div>
-            text here
+            Dashboard Page
         </div>
     );
 }
