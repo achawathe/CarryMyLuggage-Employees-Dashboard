7a30e637092cfdabde233c438cc77bd90fd5de58
achawathe <akhil.chawathe@gmail.com>
2024-11-07 19:10:43 -0500

    Prelim Backend

---
+++ carry-my-luggage-ui/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+const userRouter = require('./routes/users');
+
+app.use('/users', userRouter);
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+    }
+);
